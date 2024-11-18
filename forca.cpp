fa9256f521a13bed53abf7a861e304951c724b9a
Nathalia <110792289+ndamasc@users.noreply.github.com>
2024-11-18 16:06:23 -0400

    codigo funcionando

--- forca.cpp
+++ forca.cpp
@@ -3,52 +3,79 @@
 #include<vector>
 #include<algorithm>
 
-using namespace std; 
+
+using namespace std;
+
+//  variaveis
+
+string palavraSecreta;
+string mascara;
+
+int chances=0;
+bool perdeu = false;
+bool acertou=false;
+
+char letra;
+vector<char> tentativas;
+
+// criar um dicionario para o jogo escolher sozinho a palavra secreta
+
 
 int main(){
 
-    // variaveis 
+    cout<<"Digite a palavra secreta: "<<endl;
+    cin>>palavraSecreta;
 
-    string secretWord;
-    vector<char> chars_tempts;
-    int num_tempts = 0;
-    bool fail=false;
-    char letra;
+    mascara = string(palavraSecreta.length(), '_');
 
-    cout<<"Bem vindo ao jogo da velha!"<<endl;
-    cout<<"Digite a palavra secreta: ";
-    cin>>secretWord;
+    cout<<mascara;
 
-    while (!fail){
-        int c = secretWord.length();
+    while(!perdeu){
+
+        cout<<"\nEssa e  tentativa "<<chances+1<<endl;
+
+        cout<<"Digite uma letra: "<<endl;
+        cin>>letra;     //fazer com que ignore espacos em bracno
+
+    
+        if(find(tentativas.begin(), tentativas.end(), letra) != tentativas.end()){
+            cout<<"Opa! vc ja chutou essa letra"<<endl;
+        }else{
+            tentativas.push_back(letra);
+        }
+
+        acertou=false;
+
+        for(int i=0;i<mascara.length();i++){
+            if(letra==palavraSecreta[i]){
+                mascara[i]=letra;
+                acertou=true;
+            }              
+        }
         
-        for(int i=0;i<c;i++){
-            cout<<" _ ";
+        if(acertou) {
+            cout << "Correto!" << endl;
+        } else {
+            cout << "Errado!" << endl;
+            chances++;
         }
 
-        cout<<"\nDigite uma letra: "<<endl;
-        cin>>letra;
+        cout<<mascara;
 
-        if(find(chars_tempts.begin(), chars_tempts.end(), letra) != chars_tempts.end()) {
-            cout<<"A letra "<<letra<<" ja foi tentada!" <<endl;            
+        if(chances==6){
+            perdeu=true;
+            cout<<"\nVOCE PERDEU :( ";
+        }else if (mascara==palavraSecreta){
+            cout<<"\nVOCE GANHOU !!";
+            break;
         }
-        else{
-            chars_tempts.push_back(letra);
-        }
-
-        cout<<"as letras tentadas foram: "<<endl;
-
-        for(char i: chars_tempts){
-            cout<<i<< " , ";
-        }
-
-        num_tempts++;
-
-        if (num_tempts == 6){
-            fail = true;
-        }
-
 
     }
+
+
+
+
+
     
+
 }
