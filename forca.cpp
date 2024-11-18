481e7d85dddaace9597bd18ce8b2459838ecdb79
Nathalia <110792289+ndamasc@users.noreply.github.com>
2024-11-18 16:18:22 -0400

    modularizando codigo

--- forca.cpp
+++ forca.cpp
@@ -4,6 +4,8 @@
 #include<algorithm>
 
 
+
+
 using namespace std;
 
 //  variaveis
@@ -19,6 +21,55 @@ char letra;
 vector<char> tentativas;
 
 // criar um dicionario para o jogo escolher sozinho a palavra secreta
+
+
+void verifica_letra(){
+            if(find(tentativas.begin(), tentativas.end(), letra) != tentativas.end()){
+                cout<<"Opa! vc ja chutou essa letra"<<endl;
+            }else{
+                tentativas.push_back(letra);
+            }
+
+        }
+
+
+void verifica_palavra(){
+
+    acertou=false;
+
+    for(int i=0;i<mascara.length();i++){
+        if(letra==palavraSecreta[i]){
+            mascara[i]=letra;
+            acertou=true;
+        }              
+    }
+    
+    if(acertou) {
+        cout << "Correto!" << endl;
+    } else {
+        cout << "Errado!" << endl;
+        chances++;
+    }
+
+    cout<<mascara;
+
+
+}
+
+
+    void termina_jogo(){
+
+        if(chances==6){
+            perdeu=true;
+            cout<<"\nVOCE PERDEU :( ";
+        }else if (mascara==palavraSecreta){
+            cout<<"\nVOCE GANHOU !!";
+            perdeu=true;
+        }
+
+    }
+
+
 
 
 int main(){
@@ -37,45 +88,11 @@ int main(){
         cout<<"Digite uma letra: "<<endl;
         cin>>letra;     //fazer com que ignore espacos em bracno
 
-    
-        if(find(tentativas.begin(), tentativas.end(), letra) != tentativas.end()){
-            cout<<"Opa! vc ja chutou essa letra"<<endl;
-        }else{
-            tentativas.push_back(letra);
-        }
-
-        acertou=false;
-
-        for(int i=0;i<mascara.length();i++){
-            if(letra==palavraSecreta[i]){
-                mascara[i]=letra;
-                acertou=true;
-            }              
-        }
-        
-        if(acertou) {
-            cout << "Correto!" << endl;
-        } else {
-            cout << "Errado!" << endl;
-            chances++;
-        }
-
-        cout<<mascara;
-
-        if(chances==6){
-            perdeu=true;
-            cout<<"\nVOCE PERDEU :( ";
-        }else if (mascara==palavraSecreta){
-            cout<<"\nVOCE GANHOU !!";
-            break;
-        }
-
-    }
+        verifica_letra();
+        verifica_palavra();
+        termina_jogo();
 
 
-
-
-
-    
+    }  
 
 }
